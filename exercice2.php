a96f8f38cff6d1fdb033d4be987c86ec6d9837d8
fatoumata dansoko <fatoumatadansoko61@gmail.com>
2024-02-21 12:17:23 +0000

    Traitement de l'exercice 2

--- exercice2.php
+++ exercice2.php
@@ -6,6 +6,35 @@
     <title>Document</title>
 </head>
 <body>
+
+    <?php
+
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+
+
+$tableauSansDoublons = array();
+
+$tableau = array();
+
+$taille_tab =(int) readline("Donnez la taille du tableau");
+
+for($i = 0 ; $i < $taille_tab; $i++) {
+
+    //$Element = (int) readline ("Veuilez entrez les entiers" );
+    //$tableau [$i]=$Element;
+
+    $tableau [$i] = (int) readline ("Veuilez entrez les entiers" );
     
+    
+    
+}
+    echo ("affichage du tableau");
+     print_r($tableau);
+     $tableauSansDoublons = array_unique ($tableau);    
+    echo "le tableau sans doublon est de :";
+    print_r ($tableauSansDoublons);
+    ?>
 </body>
 </html>
